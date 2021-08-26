79d695607d33bedb6f03517be250389b47a5f4c7
Anastasiia Lialina <lialina.anastasiia@gmail.com>
2021-08-26 22:27:52 +0300

    Updates

--- static/js/main.508238c1.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-05-movies"]=this["webpackJsonpgoit-react-hw-05-movies"]||[]).push([[0],{11:function(t,e,n){"use strict";n.r(e);var c=n(1),i=n.n(c),o=n(3),s=n.n(o),r=(n(8),n(9),n(0));var a=function(){return Object(r.jsx)("div",{className:"App"})};s.a.render(Object(r.jsx)(i.a.StrictMode,{children:Object(r.jsx)(a,{})}),document.getElementById("root"))},8:function(t,e,n){},9:function(t,e,n){}},[[11,1,2]]]);
-//# sourceMappingURL=main.508238c1.chunk.js.map
