c2f57e366bd82b84f3381a45e5c7bb69aab1a8ab
Anastasiia Lialina <lialina.anastasiia@gmail.com>
2021-09-14 11:19:08 +0300

    Update views

--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -1,3 +1,46 @@
+import { useState, useEffect } from 'react';
+import { Link, useRouteMatch } from 'react-router-dom';
+import { fetchTrendingMovies } from '../services/movies-api';
+import SearchError from '../components/SearchError/SearchError';
+
+const Status = {
+  IDLE: 'idle',
+  RESOLVED: 'resolved',
+  REJECTED: 'rejected',
+};
+
 export default function HomePage() {
-  return <div>Hi, I am HomePage</div>;
+  // const { url } = useRouteMatch();
+  const [status, setStatus] = useState(Status.IDLE);
+  const [error, setError] = useState(null);
+  const [trendingMovies, setTrendingMovies] = useState([]);
+  // const query = 'dog';
+
+  useEffect(() => {
+    fetchTrendingMovies()
+    .then(data => data.results)
+    .then(results => {
+      setTrendingMovies(results);
+      setStatus(Status.RESOLVED);
+    })
+    .catch(error => {
+      setError(error);
+      setStatus(Status.REJECTED);
+    })
+  }, [])
+  
+  return (
+  <div>
+    <h1>Hi, I am HomePage</h1>
+    <h2>Trending today</h2>
+    <div>
+      {status === Status.REJECTED && <SearchError message={error.message} />}
+      {status === Status.RESOLVED && <ul>{trendingMovies.map((movie) => 
+        <li key={movie.id}>
+          <Link to={`movies/${movie.id}`}>{movie.title}</Link>
+        </li>)}
+      </ul>}
+    </div>
+  </div>
+  );
 }
