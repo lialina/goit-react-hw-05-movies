71d29eb3601e227d9459c8dfb1c8f13fe174be91
Anastasiia Lialina <lialina.anastasiia@gmail.com>
2021-09-17 09:52:52 +0300

    Updates

---
+++ static/js/not-found-view.6b68cf8c.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-05-movies"]=this["webpackJsonpgoit-react-hw-05-movies"]||[]).push([[5],{56:function(t,e,n){"use strict";n.r(e),n.d(e,"default",(function(){return o}));var i=n(1);function o(){return Object(i.jsx)("h1",{children:"404 Page not found"})}}}]);
+//# sourceMappingURL=not-found-view.6b68cf8c.chunk.js.map
